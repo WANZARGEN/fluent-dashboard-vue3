2a2be1203a347cf145e1822279ccdd6bda5ce155
Wanjin Noh <wanzargen@gmail.com>
2022-09-23 17:59:14 +0900

    feat: add draggable widgets

---
+++ src/components/Widget.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps<{
+  name: string;
+}>();
+</script>
+
+<template>
+  <div class="widget">
+    <p>Widget - {{ name }}</p>
+    <div class="widget-contents"></div>
+  </div>
+</template>
+
+<style scoped>
+.widget {
+  border: 1px solid aquamarine;
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+  margin: 0.5rem 0;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,41 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import Widget from "../components/Widget.vue";
+import { faker } from "@faker-js/faker";
+import Draggable from "vue3-draggable";
+
+const widgets = faker.datatype.array(10).map(() => ({
+  id: faker.datatype.uuid(),
+  name: faker.random.word(),
+}));
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="dashboard-layout">
+      <div class="card-list-wrapper">
+        <Draggable v-model="widgets">
+          <template #item="{ item }">
+            <Widget :name="item.name" :key="item.id" class="widget-card" />
+          </template>
+        </Draggable>
+      </div>
+    </div>
   </main>
 </template>
+
+<style scoped>
+.dashboard-layout {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin: auto;
+  .card-list-wrapper {
+    box-sizing: border-box;
+    display: flex;
+    flex-wrap: wrap;
+    position: relative;
+    width: 100%;
+    justify-content: left;
+  }
+}
+</style>
